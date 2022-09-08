c64567b9314372b9337d8304a50310c3e2ea6ac5
Joseph Munyuiri <josephmunyuiri@gmail.com>
2022-09-08 08:51:03 -0700

    C program that prints the size of various types on the computer it i

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -3,7 +3,8 @@
 /**
  * main - Entry point
  *
- * Description: prints("size of various types on the computer it is compiled and run on")
+ * Description: prints("size of various types on the computer it is compiled and 
+ * run on")
  *
  * Return: Always 0 (Success)
  */
