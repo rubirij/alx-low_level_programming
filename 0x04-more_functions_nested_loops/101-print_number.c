ccc2fc1ef5c6bfc585585d56c7348ef62900c7c2
Joseph Munyuiri <josephmunyuiri@gmail.com>
2022-09-16 02:52:23 -0700

    function that prints an integer

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -3,38 +3,20 @@
 
 /**
  * print_number - prints a number
- * @n: Input number
+ * @n: the number to print
  */
 
 void print_number(int n)
 {
-	long len, res, i, temp, expo;
-
-	res = n;
-	expo = len =  1;
-	/*Check negatives*/
-	if (res < 0)
+	unsigned int num = n;
+	if (n < 0)
 	{
-		res *= -1;
-		_putchar('-');
+		putchar('-');
+		num = -num;
 	}
-
-	/**/
-	temp = res;
-	while (temp >= 10)
+	if (num > 9)
 	{
-		len++;
-		temp /= 10;
+		print_number(num / 10);
 	}
-
-	/*Create Exponent*/
-	for (i = 1; i < len; i++)
-		expo *= 10;
-	/*Main */
-	while (expo > 1)
-	{
-		_putchar((res / expo) % 10 + '0');
-		expo /= 10;
-	}
-	_putchar(res % 10 + '0');
+	puchar(num % 10 + '0');
 }
