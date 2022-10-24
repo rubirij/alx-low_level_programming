91ec28f56c9ef06349e2a2514f7fdab9f9869c06
rubirij <josephmunyuiri@gmail.com>
2022-10-24 11:31:24 -0700

    New Push

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -8,12 +8,17 @@
  */
 size_t print_list(const list_t *h)
 {
-	size_t i;
+	size_t count = 0;
 
-	for (i = 0; h; i++)
+	while (h != NULL)
 	{
-		printf("[%d] %s\n", h->len, h->str);
+		if(h->str == NULL)
+			printf("{0}(nil)\n");
+		else
+			printf("{%d}%s\n", h->len, h->str);
 		h = h->next;
+
+		count++;
 	}
-	return (i);
+	return (count);
 }
